c1e082ccce912b6dcbf7901f99658ba81cc3d7c8
Valeriy Komlev <komlev88@gmail.com>
2022-02-05 14:25:43 +0300

    removing vite bundler in order to serve raw esm in package

---
+++ index.js
@@ -0,0 +1,42 @@
+import { useMemo, useState, useEffect } from "react";
+let isClient = typeof window !== "undefined",
+  getCurrent = (queries) => {
+    let index = queries.findIndex((query) => isClient && !query.q.matches),
+      queryIndex = (~index ? index : queries.length) - 1;
+    return queryIndex >= 0 ? queries[queryIndex].bp : void 0;
+  },
+  getResult = (queries) => {
+    let current = getCurrent(queries),
+      currentIndex = queries.findIndex((query) => query.bp === current),
+      res = queries.reduce((acc, q, index) => {
+        acc[q.bp] = currentIndex >= index;
+        return acc;
+      }, {});
+    res.current = current;
+    return res;
+  },
+  useMedia = (breakpoints) => {
+    let queries = useMemo(
+      () =>
+        Object.keys(breakpoints)
+          .sort((a, b) => +breakpoints[a] - +breakpoints[b])
+          .map((bp) => {
+            let q = isClient
+              ? window.matchMedia(`(min-width:${breakpoints[bp]}px)`)
+              : void 0;
+            return { q, bp };
+          }),
+      Object.values(breakpoints)
+    );
+    let [result, setResult] = useState(getResult(queries));
+    useEffect(() => {
+      let handler = () => setResult(getResult(queries));
+      queries.forEach((query) => query.q.addEventListener("change", handler));
+      return () =>
+        queries.forEach((query) =>
+          query.q.removeEventListener("change", handler)
+        );
+    }, [queries]);
+    return result;
+  };
+export { useMedia };
